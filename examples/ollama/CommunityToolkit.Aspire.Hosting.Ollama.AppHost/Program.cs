6c30f31954c55f1faedde861c32a43ae8b53ca62
Christian Schneider <5581662+cdschneider@users.noreply.github.com>
2025-11-12 11:31:37 -0600

    using slim while testing

--- examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
+++ examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
@@ -1,15 +1,17 @@
+using YamlDotNet.Core.Tokens;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var ollama = builder.AddOllama("ollama")
     .WithDataVolume()
-    .WithOpenWebUI();
+    .WithOpenWebUI(UseSlimVariant);
 
 var phi3 = ollama.AddModel("phi3", "phi3");
 var llama = ollama.AddHuggingFaceModel("llama", "bartowski/Llama-3.2-1B-Instruct-GGUF:IQ4_XS");
 
 var ollama2 = builder.AddOllama("ollama2")
     .WithDataVolume()
-    .WithOpenWebUI();
+    .WithOpenWebUI(UseSlimVariant);
 
 var tinyllama = ollama2.AddModel("tinyllama", "tinyllama");
 
@@ -21,3 +23,12 @@ builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_Ollama_Web>("webfron
     .WithReference(tinyllama);
 
 builder.Build().Run();
+
+static void UseSlimVariant(IResourceBuilder<OpenWebUIResource> builder)
+{
+    if (builder.Resource.TryGetLastAnnotation(out ContainerImageAnnotation? imageAnnotation)
+        && imageAnnotation is { Tag: not null } )
+    {
+        imageAnnotation.Tag += imageAnnotation.Tag.EndsWith("-slim") ? string.Empty : "-slim";
+    }
+}
